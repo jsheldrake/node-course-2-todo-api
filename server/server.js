9a4546c4a5fdaa254f04410d1eb010ff9e7b512f
Jonathan Sheldrake <jonathan.sheldrake@uk.ibm.com>
2017-08-03 11:10:30 +0100

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -89,10 +89,25 @@ app.patch('/todos/:id', (req,res) => {
     if(!todo) {
       return res.status(404).send()
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(port, ()=> {
